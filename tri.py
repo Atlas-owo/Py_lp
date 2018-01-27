4f3ac9329c61c182524103924a680f60a51689d5
Atlas-owo <atlas_w@outlook.com>
2018-01-27 23:15:06 +0800

    triangle finished, but remain question

--- tri.py
+++ tri.py
@@ -1,2 +1,15 @@
 def tri():
-    
+    a = [1,2,1]
+
+    while(True):
+        yield a
+        for x in range(len(a)-1):
+            a[x] = a[x] + a[x+1]
+        a.insert(0,1)
+        
+        
+
+t = tri()
+print(next(t))
+print(next(t))
+print(next(t))
