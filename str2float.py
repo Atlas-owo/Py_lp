cd7d24c7e5cadda105e3f2fc9e9e9a7cb908136c
Atlas-owo <atlas_w@outlook.com>
2018-01-31 20:50:37 +0800

    char2float solved, improvement remained

--- str2float.py
+++ str2float.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 def fn(m,n):
     return (10 * m) + n
 
@@ -10,15 +12,25 @@ def char2int(c):
     return d[c]
 
 def str2float(s):
-    for i in s:
+    for i in range(len(s)):
         if s[i] == '/':
-            s1 = s[0:i-1]
-            s2 = s[i+1:len(s)-1]
+            a = s[0:i]
+            b = s[i+1:len(s)]
             break
+        '''
         a = reduce(fn,map(char2int,s1))
         s2 = map(char2int,s2)
         s2.reverse()
         b = reduce(fm,s2)
-
         return a + b
-print(str2float('34/6'))
+        '''
+    print(a,b)
+    c = reduce(fn,map(char2int,a))
+    print(c)
+
+    d = (reduce(fn,map(char2int,b)))*(10**(-len(b)))
+    print(d)
+
+    return c + d
+
+print('3453/6543 is changed into',str2float('3453/6543'))
